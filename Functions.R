ca373589d83fded1fa9c8746266785fef882a399
Eric Balawejder <eric.balawejder@gmail.com>
2018-09-25 15:48:30 -0400

    Functions and help menus

---
+++ Functions.R
@@ -0,0 +1,22 @@
+# Functions used so far:
+
+?rnorm()
+rnorm(n=5, mean=10, sd=8)
+?c()
+?seq()
+seq(from=10, to=20, length.out=100)
+?rep()
+
+?print()
+
+?is.numeric()
+?is.integer()
+?is.double()
+?is.character()
+
+?typeof()
+
+?sqrt()
+?paste()
+
+#?
